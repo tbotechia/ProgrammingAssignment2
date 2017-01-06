2482edd66e0c6ec3bd8bccd787e130a52f8eab50
tbotechia <tbotechia@gmail.com>
2017-01-06 15:21:28 -0200

    mandando

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## As requested in the assignement, this function will take a matrix, creates a special matrix object, defining methods.
+##space between lines is for better visualization. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function () x
+        
+        setinv <- function(solve) inv <<- solve
+        
+        getinv <- function () inv
+        
+        list( set = set, get = get,
+              setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## in the function below, inversion is  calculated and displayed, if it hasn't been calculated before.
+## If it has, it will display a message telling that data is cached and display it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+		inv <- x$getinv()
+        
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        data <- x$get()
+		
+        inv <- solve(data, ...)
+		
+        x$setinv(inv)
+		
+        inv
 }
